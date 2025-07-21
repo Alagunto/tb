4580abfe4dc2e3ddbab367afddf33c2eea34906e
Alas <noone@noone.noone>
2025-07-21 20:12:23 +0400

    wip

--- simple_handlers.go
+++ simple_handlers.go
@@ -1,32 +1,32 @@
 package tb
 
-func NewHTMLReplyHandler(html string, opts ...interface{}) HandlerFunc {
+func HTMLReply(html string, opts ...interface{}) HandlerFunc {
 	opts = append(opts, &SendOptions{ParseMode: ModeHTML})
 	return func(c Context) error {
 		return c.Reply(html, opts...)
 	}
 }
 
-func NewMarkdownReplyHandler(markdown string, opts ...interface{}) HandlerFunc {
+func MarkdownReply(markdown string, opts ...interface{}) HandlerFunc {
 	opts = append(opts, &SendOptions{ParseMode: ModeMarkdown})
 	return func(c Context) error {
 		return c.Reply(markdown, opts...)
 	}
 }
 
-func NewTextReplyHandler(text string, opts ...interface{}) HandlerFunc {
+func TextReply(text string, opts ...interface{}) HandlerFunc {
 	return func(c Context) error {
 		return c.Reply(text, opts...)
 	}
 }
 
-func NewReplyHandler(what interface{}, opts ...interface{}) HandlerFunc {
+func ReplyHandler(what interface{}, opts ...interface{}) HandlerFunc {
 	return func(c Context) error {
 		return c.Reply(what, opts...)
 	}
 }
 
-func NewEditHandler(what interface{}, opts ...interface{}) HandlerFunc {
+func EditHandler(what interface{}, opts ...interface{}) HandlerFunc {
 	return func(c Context) error {
 		return c.Edit(what, opts...)
 	}
