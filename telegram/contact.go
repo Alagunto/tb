f8992e9a59eaf445644ded698db2cabe579e5d80
Alas <noone@noone.noone>
2025-10-31 12:45:26 +0400

    refactor(telegram): clean up contact, dice, and venue files

--- telegram/contact.go
+++ telegram/contact.go
@@ -1,10 +1,5 @@
 package telegram
 
-import (
-	"github.com/alagunto/tb/outgoing"
-	"github.com/alagunto/tb/params"
-)
-
 // Contact represents a phone contact.
 type Contact struct {
 	PhoneNumber string `json:"phone_number"`
@@ -12,20 +7,4 @@ type Contact struct {
 	LastName    string `json:"last_name,omitempty"`
 	UserID      int64  `json:"user_id,omitempty"`
 	VCard       string `json:"vcard,omitempty"`
-}
-
-// ToTelegramSendMethod implements the outgoing.Content interface.
-func (c *Contact) ToTelegramSendMethod() *outgoing.Method {
-	b := params.New()
-	b.Add("phone_number", c.PhoneNumber)
-	b.Add("first_name", c.FirstName)
-	b.Add("last_name", c.LastName)
-	b.AddInt64("user_id", c.UserID)
-	b.Add("vcard", c.VCard)
-
-	return &outgoing.Method{
-		Name:   "sendContact",
-		Params: b.Build(),
-		Files:  nil,
-	}
 }
--- telegram/venue.go
+++ telegram/venue.go
@@ -1,10 +1,5 @@
 package telegram
 
-import (
-	"github.com/alagunto/tb/outgoing"
-	"github.com/alagunto/tb/params"
-)
-
 // Venue represents a venue.
 type Venue struct {
 	Location        Location `json:"location"`
@@ -14,23 +9,4 @@ type Venue struct {
 	FoursquareType  string   `json:"foursquare_type,omitempty"`
 	GooglePlaceID   string   `json:"google_place_id,omitempty"`
 	GooglePlaceType string   `json:"google_place_type,omitempty"`
-}
-
-// ToTelegramSendMethod implements the outgoing.Content interface.
-func (v *Venue) ToTelegramSendMethod() *outgoing.Method {
-	b := params.New()
-	b.AddFloat("latitude", v.Location.Lat)
-	b.AddFloat("longitude", v.Location.Lng)
-	b.Add("title", v.Title)
-	b.Add("address", v.Address)
-	b.Add("foursquare_id", v.FoursquareID)
-	b.Add("foursquare_type", v.FoursquareType)
-	b.Add("google_place_id", v.GooglePlaceID)
-	b.Add("google_place_type", v.GooglePlaceType)
-
-	return &outgoing.Method{
-		Name:   "sendVenue",
-		Params: b.Build(),
-		Files:  nil,
-	}
 }
