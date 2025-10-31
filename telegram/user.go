5ef2b4115a52d52ba952272148fc44813022fcc0
Alas <noone@noone.noone>
2025-10-31 12:44:47 +0400

    refactor(telegram): update User struct and clean up user.go

--- telegram/user.go
+++ telegram/user.go
@@ -1,31 +1,18 @@
 package telegram
 
-import "strconv"
-
-// User object represents a Telegram user, bot.
+// User object represents a Telegram user or bot.
 type User struct {
-	ID int64 `json:"id"`
-
-	FirstName           string   `json:"first_name"`
-	LastName            string   `json:"last_name"`
-	IsForum             bool     `json:"is_forum"`
-	Username            string   `json:"username"`
-	LanguageCode        string   `json:"language_code"`
-	IsBot               bool     `json:"is_bot"`
-	IsPremium           bool     `json:"is_premium"`
-	AddedToMenu         bool     `json:"added_to_attachment_menu"`
-	Usernames           []string `json:"active_usernames"`
-	StatusCustomEmojiID string   `json:"emoji_status_custom_emoji_id"`
-
-	// Returns only in getMe
-	CanJoinGroups        bool `json:"can_join_groups"`
-	CanReadMessages      bool `json:"can_read_all_group_messages"`
-	SupportsInline       bool `json:"supports_inline_queries"`
-	CanConnectToBusiness bool `json:"can_connect_to_business"`
-	HasMainWebApp        bool `json:"has_main_web_app"`
-}
-
-// Recipient returns user id as string for sending messages.
-func (u *User) Recipient() string {
-	return strconv.FormatInt(u.ID, 10)
+	ID                      int64  `json:"id"`
+	IsBot                   bool   `json:"is_bot"`
+	FirstName               string `json:"first_name"`
+	LastName                string `json:"last_name,omitempty"`
+	Username                string `json:"username,omitempty"`
+	LanguageCode            string `json:"language_code,omitempty"`
+	IsPremium               bool   `json:"is_premium,omitempty"`
+	AddedToAttachmentMenu   bool   `json:"added_to_attachment_menu,omitempty"`
+	CanJoinGroups           bool   `json:"can_join_groups,omitempty"`
+	CanReadAllGroupMessages bool   `json:"can_read_all_group_messages,omitempty"`
+	SupportsInlineQueries   bool   `json:"supports_inline_queries,omitempty"`
+	CanConnectToBusiness    bool   `json:"can_connect_to_business,omitempty"`
+	HasMainWebApp           bool   `json:"has_main_web_app,omitempty"`
 }
